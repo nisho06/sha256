b8e3d279f90f5427fbd88d971fe61cd4f05918ff
nisho06 <nishothan.17@cse.mrt.ac.lk>
2021-01-17 12:30:43 +0530

    some changes

--- src/sha256/hashing/Implementation.java
+++ src/sha256/hashing/Implementation.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package sha256.hashing;
 
 import java.io.BufferedReader;
@@ -12,20 +30,24 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Scanner;
 
+/**
+ * This class contains the sample implementation for sha256 hashing algorithm which is used in WSO2 Identity Server.
+ */
 public class Implementation {
 
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("File name of the password");
         String passwordFile = reader.readLine();
         System.out.println("File name of the salt");
         String saltFile = reader.readLine();
         int count;
-        ArrayList<String> mySalts = readSaltValuesFromFile(saltFile+".txt");
-        ArrayList<String> myPasswords = passwordArrayInCharArray(passwordFile+".txt");
+        ArrayList<String> mySalts = readSaltValuesFromFile(saltFile + ".txt");
+        ArrayList<String> myPasswords = passwordArrayInCharArray(passwordFile + ".txt");
 
         long start = System.currentTimeMillis();
-        for (  count=0; count<10000 ; count++){
+        for (count = 0; count < 7; count++) {
             String myPassword = sha256Hash(myPasswords.get(count), mySalts.get(count));
             System.out.println(myPassword);
         }
@@ -36,10 +58,18 @@ public class Implementation {
         System.out.println(count);
     }
 
-    private static String sha256Hash(String Password, String salt) throws NoSuchAlgorithmException,
+    /**
+     *  This method is responsible for calculating the sha-256 hash value for the respective Password and Salt values.
+     * @param password The password which needs to be hashed.
+     * @param salt The salt which is unique and needs to be concatanated with passwords before it is hashed.
+     * @return The hash String value in Base64 encoding scheme.
+     * @throws NoSuchAlgorithmException thrown if there is no Message digest algorithm like "SHA-256".
+     * @throws UnsupportedEncodingException thrown if the UTF-8 character encoding is not supported.
+     */
+    private static String sha256Hash(String password, String salt) throws NoSuchAlgorithmException,
             UnsupportedEncodingException {
 
-        String passwordAndSalt = Password+salt;
+        String passwordAndSalt = password + salt;
         char[] charArrayPasswordAndSalt = passwordAndSalt.toCharArray();
 
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
@@ -48,22 +78,31 @@ public class Implementation {
         return passwordString;
     }
 
-
+    /**
+     * This method is responsible for creating a password array in char[] with the passwords read from the file.
+     * @param filename the filename which has the passwords.
+     * @return returns the char[] of array which contains all the char [] for respective passwords read from the file.
+     * @throws FileNotFoundException This exception is thrown when there is no file in the name given for filename.
+     */
     private static ArrayList<String> passwordArrayInCharArray(String filename) throws FileNotFoundException {
 
         ArrayList<String> passwordArray = new ArrayList<>();
         File myObj = new File(filename);
         Scanner myReader = new Scanner(myObj);
-        int count = 0;
         while (myReader.hasNextLine()) {
             String password = myReader.nextLine();
             passwordArray.add(password);
-            count++;
         }
         myReader.close();
         return passwordArray;
     }
 
+    /**
+     * This method is responsible for reading the salt value from a file which contains the salt in each line.
+     *
+     * @param filename the filename which has the salts.
+     * @return String array list which contains all the salt values.
+     */
     private static ArrayList<String> readSaltValuesFromFile(String filename) {
 
         ArrayList<String> salt = new ArrayList<>();
@@ -78,7 +117,7 @@ public class Implementation {
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
-            e.printStackTrace();// TODO: 2021-01-07 end the flow while FileNotFoundException
+            e.printStackTrace(); // TODO: 2021-01-07 end the flow while FileNotFoundException
         }
         return salt;
     }
